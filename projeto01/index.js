8e46f75e52bd044aa3815860accb0a5e9c4d0eed
Thiago Aquino <aquino.thiago33@gmail.com>
2020-08-03 20:58:50 -0300

    Brincando com as possibilidades, somente usuário autenticando pode acessar a rota :D

--- projeto01/index.js
+++ projeto01/index.js
@@ -1,6 +1,26 @@
 /**
  * Ferramentas utilizadas:
  * Express, SQL e EJS
+ * Projeto responsável por perguntas e respostas, como se fosse um Yahoo
  */
+
+ const express = require("express");
+ const app = express();
+
+ /**EJS como View Engine */
+ app.set('view engine', 'ejs');
+
+ app.get("/", (req, res) => {
+     let user = false;
+     if(user){
+        res.render("index");
+    } else {
+        res.render("notAuthorized")
+    }
+ });
+
+ app.listen(8080,() => {
+     console.log("App rodando na porta 8080!")
+ });
 
  
